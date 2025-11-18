3c4c0d9fe53e62fad6b14301f5bbe8e237509cfd
Rob Kennedy <brobzilla@gmai.com>
2025-11-18 17:34:33 -0500

    Udpate the starting time

--- reid/reid/src/app/app.config.ts
+++ reid/reid/src/app/app.config.ts
@@ -3,13 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {reidPrefixInterceptor} from './reid-prefix.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(
-      withInterceptors([reidPrefixInterceptor])
-    ),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
